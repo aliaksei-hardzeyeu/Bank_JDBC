932f89c278cdd18de44b58efc061c2ac4d32da53
aliaksei-hardzeyeu <fugent89@yandex.ru>
2022-06-10 12:12:06 +0300

    main commit 2 before branch

--- src/main/java/by/hardzeyeu/jdbc/bank/git_rebase_test/Main.java
+++ src/main/java/by/hardzeyeu/jdbc/bank/git_rebase_test/Main.java
@@ -1,5 +1,7 @@
 package by.hardzeyeu.jdbc.bank.git_rebase_test;
 
 public class Main {
-    //created before branching to feature
+    //created before branching to feature 1 for commit 1
+
+    //created before branching to feature 2 for commit 2
 }
