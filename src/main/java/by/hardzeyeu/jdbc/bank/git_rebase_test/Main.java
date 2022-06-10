17374179ed6fded9c07ff1551c00826d0219f49d
aliaksei-hardzeyeu <fugent89@yandex.ru>
2022-06-10 12:18:27 +0300

    feature commit 2

--- src/main/java/by/hardzeyeu/jdbc/bank/git_rebase_test/Main.java
+++ src/main/java/by/hardzeyeu/jdbc/bank/git_rebase_test/Main.java
@@ -4,4 +4,6 @@ public class Main {
     //created before branching to feature 1 for commit 1
 
     //created before branching to feature 2 for commit 2
+
+    //created after branching to feature 3 for commit 3
 }
